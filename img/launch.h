6cba3217d4792a4d0bf3990eb367c60f2d086f6a
Henry <henrythomasharris@gmail.com>
2018-04-08 20:03:19 -0700

    Remove launch

---
+++ img/launch.h
@@ -0,0 +1,23 @@
+/*
+ * Exported with nin10kit v1.7
+ * Invocation command was nin10kit --mode=3 --resize=240x160 launch launch.jpg 
+ * Time-stamp: Sunday 04/08/2018, 19:53:58
+ * 
+ * Image Information
+ * -----------------
+ * launch.jpg 240@160
+ * 
+ * All bug reports / feature requests are to be filed here https://github.com/TricksterGuy/nin10kit/issues
+ */
+
+#ifndef LAUNCH_H
+#define LAUNCH_H
+
+extern const unsigned short launch[38400];
+#define LAUNCH_SIZE 76800
+#define LAUNCH_LENGTH 38400
+#define LAUNCH_WIDTH 240
+#define LAUNCH_HEIGHT 160
+
+#endif
+
