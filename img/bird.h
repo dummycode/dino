15a35af5c1665461384accd4278dd5093046ff81
Henry <henrythomasharris@gmail.com>
2018-04-08 17:02:16 -0700

    Add image and readme

---
+++ img/bird.h
@@ -0,0 +1,23 @@
+/*
+ * Exported with nin10kit v1.7
+ * Invocation command was nin10kit --mode=3 --resize=25x25 bird bird.png 
+ * Time-stamp: Sunday 04/08/2018, 16:12:27
+ * 
+ * Image Information
+ * -----------------
+ * bird.png 25@25
+ * 
+ * All bug reports / feature requests are to be filed here https://github.com/TricksterGuy/nin10kit/issues
+ */
+
+#ifndef BIRD_H
+#define BIRD_H
+
+extern const unsigned short bird[625];
+#define BIRD_SIZE 1250
+#define BIRD_LENGTH 625
+#define BIRD_WIDTH 25
+#define BIRD_HEIGHT 25
+
+#endif
+
---
+++ readme.txt
@@ -0,0 +1,5 @@
+My version of the Google Chrome's Dino game
+
+Press "Up" to jump and attempt to avoid the enemies.
+
+Select pauses the game at any time. Pressing select again will quit, and start resumes it.
